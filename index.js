528050c162f3597522fd3282d635b0211b8fbaa9
anissa guembre <guembreani@gmail.com>
2021-05-16 08:44:16 -0400

    completed

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+
+  }
+
+  static titleize(string){
+    const excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let split = string.split(" ")
+    let words = []
+    for (let i = 0; i < split.length; i++) {
+      if (excludeWords.includes(split[i])) {
+          words.push(split[i])
+      } else {
+          words.push(this.capitalize(split[i]))
+      }
+  }
+  return this.capitalize(words.join(" "))
 }
+}    
+
+
+  
